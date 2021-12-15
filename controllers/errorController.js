3b20a5e7f839956052aac0c491cc8ca52de36711
Pekka Lehtola <pekka.lehtola98@gmail.com>
2021-12-15 14:46:04 +0200

    Css fixes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,12 +8,12 @@ exports.logErrors = (error, req, res, next) => {
 exports.errorNoResourceFound = (req, res) => {
     let errorCode = httpStatus.NOT_FOUND;
     res.status(errorCode);
-    res.sendFile(`./public/${errorCode}.html`, {root: './'});
+    res.render(`./errors/${errorCode}`);
 };
 
 exports.errorInternal = (req, res) => {
     let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
     console.log(`ERROR occurred: ${error.stack}`)
     res.status(errorCode);
-    res.send(`./public/${errorCode}.html`, {root: './'});
+    res.render(`./errors/${errorCode}`);
 };
